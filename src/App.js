3455855fd6f97a7f35e03c42d9df9ee63e483f33
Stephon Patton <stephonpatton@icloud.com>
2021-02-19 13:26:50 -0500

    - Dealing with state and rendering multiple components

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 import Tweet from './components/Tweet';
 import CreateTweet from "./components/CreateTweet";
+import React, {useState} from "react";
 
 function App() {
-    const name = "Stephon Patton";
+
+    const [textInput, setTextInput] = useState("");
+    const [tweets, setTweets] = useState([]);
+
     const message = "I went to sleep today";
     const time = new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds()
         + "\n" + new Date().toDateString();
     const textStyle = {color: 'red', fontSize: 20};
-    const alertHandler = () => {
-        alert('Hello');
+    const [name, setName] = useState("Stephon ");
+    const sayHelloHandler = (user) => {
+        if(name !== "JON JONES BONES") {
+            setName("JON JONES BONES");
+        } else {
+            setName("Stephon Patton");
+        }
+        // alert(`Hello ${user}`);
     }
+
+    const secondName = "Dev Ed";
     return (
 
     <div className="App">
       <header className="App-header">
-          <CreateTweet/>
+          <CreateTweet textInput = {textInput} setTextInput={setTextInput} tweets ={tweets} setTweets={setTweets}/>
           <Tweet name={name} message = {message}/>
+          <button onClick={() => setName("JOHN JONSSSSS")}></button>
           <h1 style={textStyle}>{time}</h1>
-          <h1>{name}</h1>
-          <button onClick={alertHandler}>
+          <h1>Hello {name}</h1>
+          {/*//Callback with () => in order to not invoke immediately*/}
+          <button onClick={() => sayHelloHandler("Stephon")}>
               CLICK THIS
           </button>
       </header>
--- src/components/CreateTweet.js
+++ src/components/CreateTweet.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
-const CreateTweet = () => {
+const CreateTweet = ({tweets, setTweets, textInput, setTextInput}) => {
+    //State
+
+
+    //Functions
+    const userInputHandler = (e) => {
+        setTextInput(e.target.value);
+    };
+
+    const submitTweetHandler = (e) => {
+        e.preventDefault();
+        setTweets([...tweets, textInput]);
+    }
+     const resetText = () => {
+         setTextInput("");
+     };
+
     return (
-        <form>\
-            <textarea cols="50" rows="5"></textarea>
+        <form onSubmit={submitTweetHandler}>
+            <textarea onChange={userInputHandler} value={textInput} cols="50" rows="5"></textarea>
             <button>Submit</button>
+            <h1 onClick={() => resetText()}> {textInput} </h1>
+            
         </form>
     );
 };
